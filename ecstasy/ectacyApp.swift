3a21283f14c10dac51da7fe4118c5bd4b00e2c89
James Wallman <jbmwall756@gmail.com>
2025-07-04 13:45:32 -0500

    Add files via upload

---
+++ ecstasy/ectacyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecstasyApp.swift
+//  ecstasy
+//
+//  Created by James Wallman on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ecstasyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
